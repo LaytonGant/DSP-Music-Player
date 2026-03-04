5e9742d336d3952ed71a9f2b9ab70101cb839167
LaytonGant <111065064+LaytonGant@users.noreply.github.com>
2026-03-03 21:09:11 -0600

    Add files via upload

---
+++ Brandify/Brandify/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Pause = New System.Windows.Forms.Button()
+        Me.ToolStripContainer1 = New System.Windows.Forms.ToolStripContainer()
+        Me.StopButton = New System.Windows.Forms.Button()
+        Me.Play = New System.Windows.Forms.Button()
+        Me.SongListBox = New System.Windows.Forms.ListBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.IndexButton = New System.Windows.Forms.Button()
+        Me.ToolStripContainer1.ContentPanel.SuspendLayout()
+        Me.ToolStripContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Pause
+        '
+        Me.Pause.Location = New System.Drawing.Point(220, 15)
+        Me.Pause.Name = "Pause"
+        Me.Pause.Size = New System.Drawing.Size(103, 32)
+        Me.Pause.TabIndex = 0
+        Me.Pause.Text = "Pause"
+        Me.Pause.UseVisualStyleBackColor = True
+        '
+        'ToolStripContainer1
+        '
+        '
+        'ToolStripContainer1.ContentPanel
+        '
+        Me.ToolStripContainer1.ContentPanel.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ToolStripContainer1.ContentPanel.Controls.Add(Me.IndexButton)
+        Me.ToolStripContainer1.ContentPanel.Controls.Add(Me.StopButton)
+        Me.ToolStripContainer1.ContentPanel.Controls.Add(Me.Play)
+        Me.ToolStripContainer1.ContentPanel.Controls.Add(Me.Pause)
+        Me.ToolStripContainer1.ContentPanel.Size = New System.Drawing.Size(803, 143)
+        Me.ToolStripContainer1.Location = New System.Drawing.Point(-2, 366)
+        Me.ToolStripContainer1.Name = "ToolStripContainer1"
+        Me.ToolStripContainer1.Size = New System.Drawing.Size(803, 168)
+        Me.ToolStripContainer1.TabIndex = 1
+        Me.ToolStripContainer1.Text = "ToolStripContainer1"
+        '
+        'ToolStripContainer1.TopToolStripPanel
+        '
+        '
+        'StopButton
+        '
+        Me.StopButton.Location = New System.Drawing.Point(477, 15)
+        Me.StopButton.Name = "StopButton"
+        Me.StopButton.Size = New System.Drawing.Size(93, 32)
+        Me.StopButton.TabIndex = 2
+        Me.StopButton.Text = "Stop"
+        Me.StopButton.UseVisualStyleBackColor = True
+        '
+        'Play
+        '
+        Me.Play.Location = New System.Drawing.Point(349, 15)
+        Me.Play.Name = "Play"
+        Me.Play.Size = New System.Drawing.Size(104, 32)
+        Me.Play.TabIndex = 1
+        Me.Play.Text = "Play"
+        Me.Play.UseVisualStyleBackColor = True
+        '
+        'SongListBox
+        '
+        Me.SongListBox.BackColor = System.Drawing.SystemColors.ControlText
+        Me.SongListBox.FormattingEnabled = True
+        Me.SongListBox.Location = New System.Drawing.Point(173, 12)
+        Me.SongListBox.Name = "SongListBox"
+        Me.SongListBox.Size = New System.Drawing.Size(615, 329)
+        Me.SongListBox.TabIndex = 3
+        '
+        'IndexButton
+        '
+        Me.IndexButton.Location = New System.Drawing.Point(735, 15)
+        Me.IndexButton.Name = "IndexButton"
+        Me.IndexButton.Size = New System.Drawing.Size(55, 32)
+        Me.IndexButton.TabIndex = 3
+        Me.IndexButton.Text = "Index"
+        Me.IndexButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SongListBox)
+        Me.Controls.Add(Me.ToolStripContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Brandify"
+        Me.ToolStripContainer1.ContentPanel.ResumeLayout(False)
+        Me.ToolStripContainer1.ResumeLayout(False)
+        Me.ToolStripContainer1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Pause As Button
+    Friend WithEvents ToolStripContainer1 As ToolStripContainer
+    Friend WithEvents StopButton As Button
+    Friend WithEvents Play As Button
+    Friend WithEvents SongListBox As ListBox
+    Friend WithEvents IndexButton As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+End Class
---
+++ Brandify/Brandify/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports System.Threading
+Public Class Form1
+    Delegate Sub ListBoxDelegate(ByVal command As Integer, ByVal myStr As String)
+    Dim ListBoxDel As New ListBoxDelegate(AddressOf ListBoxDelMethod)
+
+    Dim PC_Comm As New Thread(AddressOf PC_Comm_method)
+    Dim ShowFiles As Integer = 1
+    Dim StartFileList As Integer = 2
+    Dim EndFileList As Integer = 3
+
+    Dim ShowFilesStr As String = "1"
+    Dim StartFileListStr As String = "2"
+    Dim EndFileListStr As String = "3"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Try to open the seial port 
+        Try
+            SerialPort1.Open()
+        Catch
+            Debug.WriteLine("Failed to open serial port")
+        End Try
+        ' Make this a background thread so it automatically
+        ' aborts when the main program stops.
+        PC_Comm.IsBackground = True
+        ' Set the thread priority to lowest
+        PC_Comm.Priority = ThreadPriority.Lowest
+        ' Start the thread
+        PC_Comm.Start()
+    End Sub
+
+    Private Sub Pause_Click(sender As Object, e As EventArgs) Handles Pause.Click
+
+    End Sub
+
+    Private Sub Play_Click(sender As Object, e As EventArgs) Handles Play.Click
+
+    End Sub
+
+    Private Sub StopButton_Click(sender As Object, e As EventArgs) Handles StopButton.Click
+
+    End Sub
+
+    Private Sub SongListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles SongListBox.SelectedIndexChanged
+        If Command() = StartFileList Then
+            File_List.Items.Clear()
+        ElseIf Command() = EndFileList Then
+            File_List.Items.Add(myStr)
+        End If
+    End Sub
+
+    Private Sub IndexButton_Click(sender As Object, e As EventArgs) Handles IndexButton.Click
+
+    End Sub
+
+    Private Sub PC_Comm_method()
+        Dim str As String
+        While 1
+            If SerialPort1.IsOpen Then
+                Try
+                    str = SerialPort1.ReadLine() ' Wait for start string
+                Catch ex As Exception
+                End Try
+
+                'String.Compare return values:
+                ' Less than zero: strA precedes strB in the sort order.
+                'Zero" strA occurs in the same position as strB in the sort order.
+                'Greater than zero: strA follows strB in the sort order.
+                If Not String.Compare(str, StartFileListStr) Then
+                    ' Received a StartFileList response
+                    ' clear the list
+                    ' Use the delegate to access the ListBox
+                    File_List.Invoke(ListBoxDel, StartFileList, "")
+                    ' get next string
+                    Try
+                        str = SerialPort1.ReadLine() ' read file name
+                    Catch ex As Exception
+                    End Try
+                    While String.Compare(str, EndFileListStr)
+                        ' The read string is a file name and not the EndFileList
+                        File_List.Invoke(ListBoxDel, EndFileList, str)
+                        Try
+                            str = SerialPort1.ReadLine() ' read file name
+                        Catch ex As Exception
+                        End Try
+                    End While
+                    ' While loop ends when a 3 is received
+                End If
+            Else
+                Threading.Thread.Sleep(500)
+            End If
+        End While
+    End Sub
+    Private Sub Show_Files_Click(sender As Object, e As EventArgs) Handles Show_Files.Click
+        'Show_Files.Click()
+
+        If SerialPort1.IsOpen Then
+            ' Test for communication between Visual and Board
+            SerialPort1.Write("R", 0, 1)
+            ' Send Show_Files command
+            SerialPort1.Write(ShowFilesStr, 0, 1)
+        End If
+    End Sub
+
+    Public Sub ListBoxDelMethod(ByVal command As Integer, ByVal myStr As String)
+
+        If command = StartFileList Then
+            File_List.Items.Clear()
+        ElseIf command = EndFileList Then
+            File_List.Items.Add(myStr)
+        End If
+    End Sub
+End Class
